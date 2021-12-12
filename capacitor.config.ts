da8df9dc8fef54d7852f54362a9dbf4e935e25a6
pwejar <pwejar@gmail.com>
2021-12-12 09:24:17 +0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'qrScanner',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
